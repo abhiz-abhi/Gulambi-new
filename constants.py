0db6c2a2363315ee67565eeeedc7bc8b62c0203b
OreWaAmit <amit00kumar008@gmail.com>
2025-03-06 06:09:56 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -70,8 +70,8 @@ AFK_COMMAND_REGEX = r'^\.afk(?: |$)(.*)'  # Matches `.afk` or `.afk <message>`
 UNAFK_COMMAND_REGEX = r'^\.unafk$'  # Matches `.unafk`
 
 # Timing and Limits
-COOLDOWN = lambda: random.randint(1, 2)
-PERIODICALLY_GUESS_SECONDS = 30
+COOLDOWN = lambda: random.randint(2, 3)
+PERIODICALLY_GUESS_SECONDS = 25
 PERIODICALLY_HUNT_SECONDS = 180
 HEXA_BOT_ID = 572621020
 
